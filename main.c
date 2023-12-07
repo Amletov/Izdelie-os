72ef3019b97c6741a4aae32ae001c2c67cb07fd6
perviyd <denisperviy2019@gmail.com>
2023-12-07 23:11:33 +0300

    add menu 1th option

--- main.c
+++ main.c
@@ -19,9 +19,40 @@ int main(int, char **)
 
     }
     
-    printf("Hello, from divad!\n");
+    printf("Hi, my name is Denis Perviy. I'm a student from DPI(7000). Welcome to this FS is my course work on C lang (Ш ланг)\n");
+    printf("Please, choose an option:\n");
+    printf("1 - Init file system\n");
+    printf("2 - Mount file system\n");
+    printf("Exit - Ctrl + C\n");
 
-    init_fs(500000, "RaulRamzeS");
+    int choice = 0;
+
+    scanf("%d", &choice);
+
+    if (choice == 1)
+    {
+        printf("You have chosen an \"Init file system\" option.\n");
+        printf("Sir, enter the disk name in range of 12 characters, please: ");
+
+        char name[12];
+        scanf("%s", name);
+
+        printf("Very well.\n");
+        printf("Now, select the size of the disk: ");
+
+        int size;
+        scanf("%d", &size);
+        printf("Fine... I gotta work to do.\n");
+
+        init_fs(size, name);
+    }
+    // else (!choice)
+    // {
+    //     printf("Invalid option! P");
+    // }
+    
+
+    
     // create_disk(2049, "mazhan");
     // mount_disk("mazhan");
 
